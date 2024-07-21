bbfe39182def950ff442da26fdf2dfbd271ea035
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-07-21 18:30:18 -0300

    chore: add arquivos de configuraçao

--- src/contas/IContas.ts
+++
@@ -1,8 +0,0 @@
-export interface IContas {
-  getId(): string;
-  getTipo(): string;
-  getSaldo(): number;
-  depositar(valor: number): void;
-  sacar(valor: number): boolean;
-  transferir(contaDestino: IContas, valor: number): boolean;
-}
