a41e89ec9b86149e0f145ee0a6371252834338b3
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-07-24 11:49:45 -0300

    fix: Atualizações de configuração e módulos principais

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'true',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'linebreak-style': ['error', 'unix'],
   },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
+import { ClienteController } from './cliente/cliente.controller';
+import { ClienteService } from './cliente/cliente.service';
+import { GerenteController } from './gerente/gerente.controller';
+import { GerenteService } from './gerente/gerente.service';
+import { GerenteModule } from './gerente/gerente.module';
+import { ContaModule } from './conta/conta.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ClienteModule, GerenteModule, ContaModule],
+  controllers: [AppController, ClienteController, GerenteController],
+  providers: [AppService, ClienteService, GerenteService],
 })
 export class AppModule {}
