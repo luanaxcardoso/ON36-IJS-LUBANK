c807e4bb5e1384a48dd201fa6f09008f47444a6f
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-06 14:47:31 -0300

    feat: atualizaçao da model gerente

--- src/models/gerente.model.ts
+++ src/models/gerente.model.ts
@@ -3,6 +3,9 @@ import { Cliente } from '../models/cliente.model';
 import { Conta } from '../models/contas/conta.model';
 
 export class Gerente implements InterfacePessoa {
+  public clientes: Cliente[] = [];
+  public contas: Conta[] = [];
+
   constructor(
     public id: number,
     public nome: string,
@@ -13,17 +16,11 @@ export class Gerente implements InterfacePessoa {
     public cidade: string,
     public estado: string,
     public cpf: string,
-    public rendaSalarial?: number,  
-    public statusAtivo: boolean = true,  
-    public clientes: Cliente[] = [],    
-    public contas: Conta[] = []         
+    public rendaSalarial?: number,
+    public statusAtivo: boolean = true,
   ) {}
 
   adicionarCliente(cliente: Cliente): void {
-    if (!this.clientes) {
-      this.clientes = [];
-    }
     this.clientes.push(cliente);
   }
-
 }
