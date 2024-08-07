ff89397dda19b31b89aa94d73395ae3c676e0b68
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-07 11:11:23 -0300

    test: add supertest em cliente controller - post e get

--- test/cliente.controller.spec.ts
+++ test/cliente.controller.spec.ts
@@ -1,88 +1,80 @@
+import * as supertest from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ClienteController } from '../src/controllers/cliente.controller';
-import { ClienteService } from '../src/services/cliente.service';
-import { Cliente } from '../src/models/cliente.model';
-import { NotFoundException } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 
-describe('ClienteController', () => {
-  let controller: ClienteController;
-  let service: ClienteService;
+describe('ClienteController (e2e)', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const mockClienteService = {
-      adicionarCliente: jest.fn(),
-      associarConta: jest.fn(),
-      buscarCliente: jest.fn(),
-    };
-
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ClienteController],
-      providers: [
-        {
-          provide: ClienteService,
-          useValue: mockClienteService,
-        },
-      ],
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    controller = module.get<ClienteController>(ClienteController);
-    service = module.get<ClienteService>(ClienteService);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  it('Chama o método adicionarCliente', () => {
-    const cliente = new Cliente(
-      1,
-      'Luana Cardoso',
-      '1986-08-12',
-      'luana.cardoso@gmail.com',
-      '+55 11 99856-1234',
-      'Rua Machado de Assis, 123, Apto 45',
-      'São Paulo',
-      'SP',
-      '365.968.456-00',
-      3000,
-      true
-    );
+  it('/cliente/adicionar (POST)', async () => {
+    const response = await supertest(app.getHttpServer())
+      .post('/cliente/adicionar')
+      .send({
+        nome: 'Luana Cardoso',
+        dataNascimento: '1986-08-12',
+        email: 'luana@gmail.com',
+        telefone: '11 99856-1234',
+        endereco: 'Rua Machado de Assis, 123, Apto 45',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        cpf: '365.968.456-00',
+        rendaSalarial: 3000,
+        statusAtivo: true,
+        conta: []
+      })
+      .expect(201) 
+      .expect('Content-Type', /json/);
 
-    controller.adicionarCliente(cliente);
-    expect(service.adicionarCliente).toHaveBeenCalledWith(cliente);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.nome).toBe('Luana Cardoso');
+    
+    return response.body.id;
   });
 
-  it('Chama o método associarConta', () => {
-    const clienteId = 1;
-    const contaId = 1;
-    const body = { clienteId, contaId };
-    controller.associarConta(body);
-    expect(service.associarConta).toHaveBeenCalledWith(clienteId, contaId);
+  it('/cliente/:id (GET)', async () => {
+    
+    const addResponse = await supertest(app.getHttpServer())
+      .post('/cliente/adicionar')
+      .send({
+        nome: 'Luana Cardoso',
+        dataNascimento: '1986-08-12',
+        email: 'luana@gmail.com',
+        telefone: '11 99856-1234',
+        endereco: 'Rua Machado de Assis, 123, Apto 45',
+        cidade: 'São Paulo',
+        estado: 'SP',
+        cpf: '365.968.456-00',
+        rendaSalarial: 3000,
+        statusAtivo: true,
+        conta: []
+      })
+      .expect(201)
+      .expect('Content-Type', /json/);
+  
+    const clienteId = addResponse.body.id;
+  
+    const response = await supertest(app.getHttpServer())
+      .get(`/cliente/${clienteId}`)
+      .expect(200)
+      .expect('Content-Type', /json/);
+  
+    console.log('Resposta do GET:', response.body);
+  
+    expect(response.body).toHaveProperty('id', clienteId);
+    expect(response.body.nome).toBe('Luana Cardoso');
   });
+  
 
-  it('Chama o método buscarCliente', async () => {
-    const id = 1;
-    const cliente = new Cliente(
-      1,
-      'Luana Cardoso',
-      '1986-08-12',
-      'luana.cardoso@gmail.com',
-      '+55 11 99856-1234',
-      'Rua Machado de Assis, 123, Apto 45',
-      'São Paulo',
-      'SP',
-      '365.968.456-00',
-      3000,
-      true
-    );
-
-    (service.buscarCliente as jest.Mock).mockResolvedValue(cliente);
-
-    const result = await controller.buscarCliente(id);
-    expect(service.buscarCliente).toHaveBeenCalledWith(id);
-    expect(result).toEqual(cliente);
-  });
-
-  it('Deve lançar uma exceção ao buscar um cliente inexistente', async () => {
-    const id = 111;
-    (service.buscarCliente as jest.Mock).mockResolvedValue(null);
-
-    await expect(controller.buscarCliente(id)).rejects.toThrowError(new NotFoundException(`Cliente com ID ${id} não encontrado.`));
+  afterAll(async () => {
+    await app.close();
   });
 });
