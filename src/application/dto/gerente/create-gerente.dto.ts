e6568a3bc57c823dc91fa67fde4fb1a7cedad8d9
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-19 17:46:53 -0300

    feat(dto/gerente): cria diretório para DTOs relacionados a gerente

---
+++ src/application/dto/gerente/create-gerente.dto.ts
@@ -0,0 +1,49 @@
+import { IsNotEmpty, IsEmail, IsNumber, IsString, IsMobilePhone, Length, IsBoolean, Min, IsArray, IsOptional } from 'class-validator';
+import { Conta } from '../../../domain/models/contas/conta.model';
+import { InterfacePessoa } from '../../../domain/interfaces/pessoa.interface';
+
+export class CreateGerenteDto implements InterfacePessoa {
+    @IsOptional()
+    @IsNumber({}, { message: 'O ID deve ser um número.' })
+    id?: number;
+
+    @IsNotEmpty({ message: 'O nome é obrigatório.' })
+    @IsString()
+    nome: string;
+
+    @IsString()
+    dataNascimento: string;
+    
+    @IsEmail({}, { message: 'Email inválido.' })
+    email: string;
+
+
+    @IsMobilePhone('pt-BR')
+    telefone: string;
+
+    @IsString()
+    endereco: string;
+
+    @IsString()
+    cidade: string;
+
+    @IsString()
+    estado: string;
+    
+    @IsString()
+    cep: string;
+
+    @IsString()
+    cpf: string;
+
+    @IsOptional()
+    @IsNumber({}, { message: 'A renda salarial deve ser um número.' })
+    rendaSalarial?: number;
+
+    @IsNotEmpty({ message: 'O status ativo é obrigatório.' })
+    @IsBoolean({ message: 'O status ativo deve ser verdadeiro ou falso.' })
+    statusAtivo: boolean;
+
+    @IsArray({ message: 'Deve ser uma lista de contas.' })
+    conta: Conta[];
+}
---
+++ src/application/dto/gerente/update-gerente.dto.ts
@@ -0,0 +1,55 @@
+import { IsOptional, IsString, IsNumber, IsBoolean, IsArray, Min, Length } from 'class-validator';
+import { Conta } from '../../../domain/models/contas/conta.model';
+
+export class UpdateGerenteDto {
+  @IsOptional()
+  @IsString()
+  nome?: string;
+
+  @IsOptional()
+  @IsString()
+  dataNascimento?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  telefone?: string;
+
+  @IsOptional()
+  @IsString()
+  endereco?: string;
+
+  @IsOptional()
+  @IsString()
+  cidade?: string;
+
+  @IsOptional()
+  @IsString()
+  estado?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(8, 8, { message: 'O CEP deve ter 8 caracteres.' })
+  cep?: string;
+
+  @IsOptional()
+  @IsString()
+  cpf?: string;
+
+  @IsOptional()
+  @IsNumber({}, { message: 'A renda salarial deve ser um número.' })
+  @Min(0, { message: 'A renda salarial não pode ser negativa.' })
+  rendaSalarial?: number;
+
+  @IsOptional()
+  @IsBoolean({ message: 'O status ativo deve ser verdadeiro ou falso.' })
+  statusAtivo?: boolean;
+
+  @IsOptional()
+  @IsArray({ message: 'Contas deve ser uma lista de contas.' })
+  conta?: Conta[];
+}
+
