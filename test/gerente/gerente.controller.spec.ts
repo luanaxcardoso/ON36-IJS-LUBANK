260bc67ae59177c96e4213c1a60be4d97196de8f
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-19 18:00:01 -0300

    test: ajustar testes de Gerente - corrigir casos de teste em gerente.controller.spec.ts e gerente.service.spec.ts

--- test/gerente/gerente.controller.spec.ts
+++ test/gerente/gerente.controller.spec.ts
@@ -2,6 +2,8 @@ import * as supertest from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../../src/app.module';
+import { CreateGerenteDto } from '../../src/application/dto/gerente/create-gerente.dto'; // Importando o DTO
+import { UpdateGerenteDto } from '../../src/application/dto/gerente/update-gerente.dto'; // Importando o DTO
 
 describe('GerenteController (e2e)', () => {
   let app: INestApplication;
@@ -17,27 +19,30 @@ describe('GerenteController (e2e)', () => {
   });
 
   it('/gerente/criar (POST)', async () => {
+    const createGerenteDto: CreateGerenteDto = {
+      nome: 'Bernadete Alves',
+      dataNascimento: '1986-08-12',
+      email: 'bernadete@gmail.com',
+      telefone: '11 98765-4321',
+      endereco: 'Rua das Flores, 123',
+      cidade: 'São Paulo',
+      estado: 'SP',
+      cep: '12246001',
+      cpf: '123.456.789-00',
+      rendaSalarial: 5000,
+      statusAtivo: true,
+      contas: [], 
+    };
+
     const response = await supertest(app.getHttpServer())
       .post('/gerente/criar')
-      .send({
-        nome: 'Bernadete Alves',
-        dataNascimento: '1986-08-12',
-        email: 'bernadete@gmail.com',
-        telefone: '11 98765-4321',
-        endereco: 'Rua das Flores, 123',
-        cidade: 'São Paulo',
-        estado: 'SP',
-        cep: '12246001',
-        cpf: '123.456.789-00',
-        statusAtivo: true,
-        contas: [],
-      })
+      .send(createGerenteDto)
       .expect(201)
       .expect('Content-Type', /json/);
 
     expect(response.body).toHaveProperty('id');
     gerenteId = response.body.id;
-    expect(response.body.nome).toBe('Bernadete Alves');
+    expect(response.body.nome).toBe(createGerenteDto.nome);
   });
 
   it('/gerente/:id (GET)', async () => {
@@ -53,9 +58,13 @@ describe('GerenteController (e2e)', () => {
   });
 
   it('/gerente/:id (PATCH)', async () => {
+    const updateGerenteDto: UpdateGerenteDto = {
+      nome: 'Bernadete Serafim Alves',
+    };
+
     await supertest(app.getHttpServer())
       .patch(`/gerente/atualizar/${gerenteId}`)
-      .send({ nome: 'Bernadete Serafim Alves' })
+      .send(updateGerenteDto)
       .expect(200)
       .expect('Content-Type', /json/);
 
@@ -65,7 +74,7 @@ describe('GerenteController (e2e)', () => {
       .expect('Content-Type', /json/);
 
     expect(updatedResponse.body).toHaveProperty('id', gerenteId);
-    expect(updatedResponse.body.nome).toBe('Bernadete Serafim Alves');
+    expect(updatedResponse.body.nome).toBe(updateGerenteDto.nome);
   });
 
   afterAll(async () => {
