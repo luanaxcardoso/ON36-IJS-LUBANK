a9be68c6eb877d95c054ff08368b66e884a0a4c6
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-20 16:03:13 -0300

    chore: atualizado módulo de Cliente para usar ViaCepService corretamente

--- src/modules/cliente.module.ts
+++ src/modules/cliente.module.ts
@@ -3,11 +3,12 @@ import { HttpModule } from '@nestjs/axios';
 import { ClienteService } from '../application/services/cliente.service';
 import { ClienteController } from '../adapters/controllers/cliente.controller';
 import { ViaCepService } from '../application/services/viacep.service'; 
+import { ViaCepModule } from '../modules/viacep.module';
 import { ContaModule } from './conta.module';
 import { ContaService } from '../application/services/conta.service';
 
 @Module({
-  imports: [HttpModule, ContaModule],
+  imports: [HttpModule, ContaModule, ViaCepModule],
   controllers: [ClienteController],
   providers: [ClienteService, ViaCepService, ContaService],
   exports: [ClienteService, ViaCepService],
