e5f4dc6b598286602ce4d25fdfe212727c066d6f
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-08-29 17:46:16 -0300

    feat: atualização de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { Gerente } from './db/entities/gerente.entity';
       host: process.env.DB_HOST || 'localhost', 
       port: parseInt(process.env.DB_PORT, 10) || 5432,
       username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD || '*****',
       database: process.env.DB_NAME || 'lubank',
       entities: [Cliente, Conta, Gerente],
       autoLoadEntities: true, 
