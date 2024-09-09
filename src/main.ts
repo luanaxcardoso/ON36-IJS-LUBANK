9a5fd6523104ee20b618ee4d1b3ef934db9e1399
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-09-09 15:19:48 -0300

    feat: inicializa DataSource e configurações no bootstrap do main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AppDataSource } from './database/data-source'; 
 
 async function bootstrap() {
+  
+  try {
+    await AppDataSource.initialize();
+    console.log('Data Source inicializada!');
+  } catch (err) {
+    console.error('Erro durante a initialização:', err);
+    process.exit(1); 
+  }
+
   const app = await NestFactory.create(AppModule);
   
-  
   app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
+  console.log('Aplicação rodando em: http://localhost:3000');
 }
+
 bootstrap();
